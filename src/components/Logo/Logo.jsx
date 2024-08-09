eb5cbd01e370c3f566b711ba53a16cc196c74ec0
Mykhailo <matsyshynmykhailo@gmail.com>
2024-08-09 23:46:40 +0300

    fix logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,16 +1,20 @@
+import { Link } from "react-router-dom";
+import logo from "../../../public/logo.svg";
 import css from "./Logo.module.css";
 
 export default function Logo() {
   return (
-    <div className={css.position}>
-      <img
-        src="../../../public/logo.svg"
-        alt="Wander Wheels Logo Icon"
-        className={css.logoIcon}
-        width="46"
-        height="32"
-      />
-      <p>Wander Wheels</p>
-    </div>
+    <Link to="/" className={css.logoLink}>
+      <div className={css.position}>
+        <img
+          src={logo}
+          alt="Wander Wheels Logo Icon"
+          className={css.logoIcon}
+          width="46"
+          height="32"
+        />
+        <p>Wander Wheels</p>
+      </div>
+    </Link>
   );
 }
