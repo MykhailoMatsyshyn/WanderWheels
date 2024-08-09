9b5dfc58f78c7e7fa3269f841d4c4495b6dd65cc
Mykhailo <matsyshynmykhailo@gmail.com>
2024-08-09 17:39:19 +0300

    fix header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
+import { useLocation } from "react-router-dom";
 import Logo from "../Logo/Logo";
 import Navigation from "../Navigation/Navigation";
 import css from "./Header.module.css";
 
 export default function Header() {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
-    <header className={css.header}>
-      <Logo />
-      <Navigation />
+    <header className={`${css.header} ${isHomePage ? css.home : ""}`}>
+      <div className={css.headerContent}>
+        <Logo />
+        <Navigation />
+      </div>
     </header>
   );
 }
