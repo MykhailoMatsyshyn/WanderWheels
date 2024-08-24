4d7d514faf5c5917c844e972e8b884501a179c38
Mykhailo <matsyshynmykhailo@gmail.com>
2024-08-24 22:59:45 +0300

    fix

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../../../public/logo.svg";
+import logo from "../../../public/logo-camper.svg";
 import css from "./Logo.module.css";
 
 export default function Logo() {
